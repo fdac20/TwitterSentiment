5f7f75317d052abc5181d1c59ab32fafa9b30162
vickit144 <wph612@vols.utk.edu>
2020-11-05 19:42:23 +0000

    combined all csv data files

---
+++ Scripts/combine_csv.py
@@ -0,0 +1,15 @@
+import os
+import glob
+import pandas as pd
+
+os.chdir("../Datasets/University/CSV")
+
+extension = 'csv'
+all_filenames = [i for i in glob.glob('*.{}'.format(extension))]
+
+#combine all files in the list
+combined_csv = pd.concat([pd.read_csv(f) for f in all_filenames])
+
+#export to csv
+combined_csv.to_csv("combined_csv.csv", index = False)
+
