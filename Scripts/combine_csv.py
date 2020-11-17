677f79970ba87258b4d519e7e1556ef47e406e0a
vtang144 <wph612@vols.utk.edu>
2020-11-16 21:17:04 -0500

    Added new Non-Uni dataset

--- Scripts/combine_csv.py
+++ Scripts/combine_csv.py
@@ -11,5 +11,5 @@ all_filenames = [i for i in glob.glob('*.{}'.format(extension))]
 combined_csv = pd.concat([pd.read_csv(f) for f in all_filenames])
 
 #export to csv
-combined_csv.to_csv("combinedNonUni1.csv", index = False)
+combined_csv.to_csv("combinedNonUniLessSkewed.csv", index = False)
 
